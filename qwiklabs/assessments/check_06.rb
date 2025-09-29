f32b8b765b7e7b520d6d92ea31663833c6df3f28
seoeunbae <baeseoeun1221@gmail.com>
2025-09-29 16:11:19 +0900

    refactor : change checkpoint_7 to checkpoint_6

--- qwiklabs/assessments/check_06.rb
+++ qwiklabs/assessments/check_06.rb
@@ -1,12 +1,32 @@
 # method description
 def check_06(handles:, maximum_score:, resources:)
-  bigquery = handles['project_0.BigqueryV2']
+  bigquery = handles['project_0.LoggingV2']
   ret_hash = { :score => 0, :message => "", :student_message => "" }
   isAvailable = true
+
+   # last task filter
+  log_filters =[
+    'protoPayload.serviceName="bigquery.googleapis.com"',
+    'protoPayload.serviceData.jobGetQueryResultsResponse.job.jobStatistics.referencedTables.tableId="multimodal_customer_reviews"',
+    'protoPayload.serviceData.jobGetQueryResultsResponse.job.jobConfiguration.query.statementType="SELECT"'
+  ]
+
+  # Build log filter
+  custom_filter = log_filters.join(" AND ")
+
+  # List_log_entries_request_object
+  list_log_entries_request_object = Google::Apis::LoggingV2::ListLogEntriesRequest.new
+
+  # Parent resource to track
+  list_log_entries_request_object.resource_names = ['projects/' + logging.project]
+
+  list_log_entries_request_object.filter = custom_filter
+  query_logs = logging.list_entry_log_entries(list_log_entries_request_object)&.entries || []
+
   if isAvailable 
-    ret_hash = { :done => true, :score => maximum_score, :message => "Assessment completed!", :student_message => "step3 completed!"}
+    ret_hash = { :done => true, :score => maximum_score, :message => "Assessment completed!", :student_message => "All Hands-on Step completed!"}
   else
-    error_message = 'Please create the BigQuery dataset.'
+    error_message = 'Please try all the Hands-on steps again.'
     ret_hash[:message] = error_message
     ret_hash[:student_message] = error_message
   end
