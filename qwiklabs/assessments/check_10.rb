45cacbb16e5647885f339976e13eb54f83b379b3
Mooje Kong <mjkong@google.com>
2025-10-01 09:08:20 +0900

    modify checkpoint code

--- qwiklabs/assessments/check_10.rb
+++ qwiklabs/assessments/check_10.rb
@@ -2,11 +2,24 @@
 def check_10(handles:, maximum_score:, resources:)
   bigquery = handles['project_0.BigqueryV2']
   ret_hash = { :score => 0, :message => "", :student_message => "" }
-  isAvailable = true
+  isAvailable = false
+
+  begin
+    negative_customers_list_object = bigquery.get_table("cymbal".to_s, "negative_customers_list".to_s ) || []
+    negative_customer_segment_data_object = bigquery.get_table("cymbal".to_s, "negative_customer_segment_data".to_s ) || []
+  rescue 
+    isAvailable = false
+  end 
+
+  if negative_customers_list_object && negative_customer_segment_data_object
+    isAvailable = true
+  end 
+
   if isAvailable 
-    ret_hash = { :done => true, :score => maximum_score, :message => "Assessment completed!", :student_message => "step3 completed!"}
+    success_message = "테이블이 정상적으로 생성되었습니다."
+    ret_hash = { :done => true, :score => maximum_score, :message => success_message, :student_message => success_message}
   else
-    error_message = 'Please create the BigQuery dataset.'
+    error_message = "다시 한 번 절차를 확인해보세요!"
     ret_hash[:message] = error_message
     ret_hash[:student_message] = error_message
   end
